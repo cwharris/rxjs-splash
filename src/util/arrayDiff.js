e70b8f38af4101130ac113eecab3fe81d7d3b576
Christopher Harris <xixonia@gmail.com>
2013-09-08 17:34:28 -0500

    implement arrayDiff. stable.

--- src/util/arrayDiff.js
+++ src/util/arrayDiff.js
@@ -1,3 +1,19 @@
-    sx.util.arrayDiff = function (template) {
-      return [];
-    };
+    (function () {
+
+      var containsItemFrom    = function (item) { return this.indexOf(item) !== -1; };
+      var notContainsItemFrom = function (item) { return this.indexOf(item) === -1; };
+      var toStatus = function (item) { return { value: item, status: this.toString() }; };
+
+      // arrayDiff assumes no duplicate items
+
+      sx.util.arrayDiff = function (a, b) {
+
+        var added     = b.filter(notContainsItemFrom, a).map(toStatus, 'added');
+        var retained  = b.filter(containsItemFrom,    a).map(toStatus, 'retained');
+        var removed   = a.filter(notContainsItemFrom, b).map(toStatus, 'removed');
+
+        return added.concat(retained, removed);
+      };
+
+    })();
+    
--- tests/util.arrayDiff.js
+++ tests/util.arrayDiff.js
@@ -2,19 +2,115 @@
 
   module('util.arrayDiff');
 
-  test('can diff empty w/empty', function () {
+  test('can diff no changes', function () {
     expect(1);
 
-    var a = [];
-    var b = [];
+    var current = [];
+    var next = [];
 
-    var diff = sx.util.arrayDiff(a, b);
+    var diff = sx.util.arrayDiff(current, next);
 
     equal(diff.length, 0);
   });
 
-  test('can diff empty w/non-empty', function () {
-    expect(1);
+  test('can diff added items', function () {
+    expect(4);
+
+    var current = [];
+    var next = ['a', 'b', 'c'];
+
+    var diff = sx.util.arrayDiff(current, next);
+
+    equal(diff.length, 3);
+
+    var byValue = function (item) {
+      return item.value === this.toString();
+    };
+
+    var a = diff.filter(byValue, 'a')[0];
+    equal(a.status, 'added');
+
+    var b = diff.filter(byValue, 'b')[0];
+    equal(b.status, 'added');
+
+    var c = diff.filter(byValue, 'c')[0];
+    equal(c.status, 'added');
+
+  });
+
+  test('can diff retained items', function () {
+    expect(4);
+
+    var current = ['a', 'b', 'c'];
+    var next = ['a', 'b', 'c'];
+
+    var diff = sx.util.arrayDiff(current, next);
+
+    equal(diff.length, 3);
+
+    var byValue = function (item) {
+      return item.value === this.toString();
+    };
+
+    var a = diff.filter(byValue, 'a')[0];
+    equal(a.status, 'retained');
+
+    var b = diff.filter(byValue, 'b')[0];
+    equal(b.status, 'retained');
+
+    var c = diff.filter(byValue, 'c')[0];
+    equal(c.status, 'retained');
+
+  });
+
+  test('can diff removed items', function () {
+    expect(4);
+
+    var current = ['a', 'b', 'c'];
+    var next = [];
+
+    var diff = sx.util.arrayDiff(current, next);
+
+    equal(diff.length, 3);
+
+    var byValue = function (item) {
+      return item.value === this.toString();
+    };
+
+    var a = diff.filter(byValue, 'a')[0];
+    equal(a.status, 'removed');
+
+    var b = diff.filter(byValue, 'b')[0];
+    equal(b.status, 'removed');
+
+    var c = diff.filter(byValue, 'c')[0];
+    equal(c.status, 'removed');
+
+  });
+
+  test('can diff mixed items', function () {
+    expect(4);
+
+    var current = ['a', 'b'];
+    var next = ['b', 'c'];
+
+    var diff = sx.util.arrayDiff(current, next);
+
+    equal(diff.length, 3);
+
+    var byValue = function (item) {
+      return item.value === this.toString();
+    };
+
+    var a = diff.filter(byValue, 'a')[0];
+    equal(a.status, 'removed');
+
+    var b = diff.filter(byValue, 'b')[0];
+    equal(b.status, 'retained');
+
+    var c = diff.filter(byValue, 'c')[0];
+    equal(c.status, 'added');
+
   });
 
 }(typeof global == 'object' && global || this));
